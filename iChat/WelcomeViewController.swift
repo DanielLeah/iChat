4642b7271e973376895faa9674fc8cc2d5df3abb
Daniel Leah <daniel.leah94@gmail.com>
2019-07-04 11:36:18 +0100

    Initial Commit

---
+++ iChat/WelcomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  WelcomeViewController.swift
+//  iChat
+//
+//  Created by David Daniel Leah (BFS EUROPE) on 04/07/2019.
+//  Copyright © 2019 David Daniel Leah (BFS EUROPE). All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet var emailTextField: UIView!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatTextField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    //MARK: Actions
+    @IBAction func loginTapped(_ sender: Any) {
+    }
+    @IBAction func registerTapped(_ sender: Any) {
+    }
+    @IBAction func oneTapRecognizer(_ sender: Any) {
+    }
+    
+    
+    
+
+}
